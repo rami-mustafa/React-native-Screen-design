717115cf685f34dab25fb700777c2f07c7f6d7a2
rami-mustafa <ramimustafa77@gmail.com>
2021-05-14 23:11:15 +0300

    ..

--- UI1.js
+++ UI1.js
@@ -1,23 +1,92 @@
 import React from 'react';
- import {View , Text , StyleSheet , TouchableOpacity , Dimensions , Image, StatusBar} from 'react-native';
+ import {View , Text , StyleSheet , TouchableOpacity , Dimensions , Image, StatusBar, TextInput} from 'react-native';
 
 const color1 ='#ff5948'; 
 const color2 ='#f8c41c'; 
 
-
+  
 const W = Dimensions.get('window').width;
 const H = Dimensions.get('window').height;
 
 
-class UI1 extends React.Component
- {
+class UI1 extends React.Component {
+
+     state ={
+          page : ''
+         
+     };
+      
+     signUpOnPress = () => {
+              
+          
+          this.setState({page :'signUp'});
+     }
+     signInOnPress = () => {
+              
+          this.setState({page :'signIn'});
+     }
+ 
+
+
+
+
+
+    buttons() {
+       return(
+
+          <View style = {stil. buttonContainer}>
+
+          <TouchableOpacity 
+               style = {[stil.signUpButton , stil.Button]}
+               onPress={this.signUpOnPress}
+               >
+               <Text style = {stil.signUpButtonText}> SING UP </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity 
+               style = {[ stil.signInButton  , stil.Button]}
+               onPress={this.signInOnPress}
+          >
+               <Text style = {stil.signInButtonText}> SING IN </Text>
+          </TouchableOpacity>
+
+          </View>
+       )
+    }
+
+    signUp (){
+         return(
+              <Text>signUp</Text>
+         );
+    }
+
+    signIn (){
+     return(
+            
+          <View style = {stil.signInContainer}>
+              <View style = {stil.InputContainer}>
+                  <Text style = {stil.inputCaption}>Kullanıccı </Text>
+                  <TextInput
+                  style = {stil.input}
+                  />
+
+
+              </View>
+
+
+          </View>
+
+
+
+     );
+}
+    
+    
     render() {
 
-     return (
-           <View style={stil.mainContainer} >
-                <StatusBar translucent backgroundColor={'transparent'}     />
-
-               
+      return (
+             <View style={stil.mainContainer} >
+                  <StatusBar translucent backgroundColor={'transparent'}     />
 
                 <View style = {stil.topContainer}>
                      <Image 
@@ -27,24 +96,16 @@ class UI1 extends React.Component
                      />
                 </View>
 
- 
-               <View style = {stil.bottomContainer}>    
+                <View style = {stil.bottomContainer}>    
+                  {this.state.page === ''       && this.buttons()}
+                  {this.state.page === 'signUp' && this.signUp() }
+                  {this.state.page === 'signIn' && this.signIn() }
+                </View>
 
-                     <View style = {stil. buttonContainer}>
-
-                         <TouchableOpacity style = {[stil.signUpButton , stil.Button]}>
-                              <Text style = {stil.signUpButtonText}> SING UP </Text>
-                         </TouchableOpacity>
-
-                         <TouchableOpacity style = {[ stil.signInButton  , stil.Button]}>
-                              <Text style = {stil.signInButtonText}> SING IN </Text>
-                         </TouchableOpacity>
-
-                     </View>
-               </View>
            </View>
        );
    }
+
 }
 
 const stil = StyleSheet.create ({
@@ -104,6 +165,28 @@ const stil = StyleSheet.create ({
           color : 'white',
      },
 
+
+     signInContainer : {
+               borderWidth : 1,
+               alignItems : 'center',
+     },
+     InputContainer : {
+          borderWidth : 1,
+         
+     },
+     
+     input : {
+          width  : W * 0.8,
+          borderBottomWidth : 1.5,
+          borderBottomColor : 'white',
+          fontWeight: 'bold',
+          color : 'white' ,
+     },
+     
+     inputCaption : {
+          color : color2 ,
+          fontWeight: 'bold',
+     },
 
 });
 
